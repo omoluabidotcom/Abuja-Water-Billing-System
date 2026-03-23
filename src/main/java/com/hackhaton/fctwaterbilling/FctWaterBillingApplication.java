fc36c6ddd0b31bbb1fee836b592b82d162ae0f17
omoluabidotcom <yyahaya222@yahoo.com>
2026-03-23 14:07:21 +0100

    Initial commit

---
+++ src/main/java/com/hackhaton/fctwaterbilling/FctWaterBillingApplication.java
@@ -0,0 +1,13 @@
+package com.hackhaton.fctwaterbilling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FctWaterBillingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FctWaterBillingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hackhaton/fctwaterbilling/FctWaterBillingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackhaton.fctwaterbilling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FctWaterBillingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
